3c408099116e0d95fdac1646a0332da41f002377
Deaano <67870671+Deaano@users.noreply.github.com>
2021-07-19 13:11:32 +0100

    finish contact section

--- src/compnents/Contact.jsx
+++ src/compnents/Contact.jsx
@@ -14,17 +14,19 @@ function Contact() {
 
     if (!contactSection){
     return (
+        <div className='container-cover'>
         <div className='contact-container'>
            <h2 className='contact-title'>Like What You See? Get In Touch.</h2>
            <div className='contact-button-container'>
             <button className='contact-button' onClick={clickHandler}>Click Here</button>
             </div>
         </div>
+        </div>
     )
     } else {
         return(
            
-            
+            <div className='container-cover'>
             <div className='contact-container'>
            <h2 className='clicked-title'>Contact me at any time of the day through either of these ways :)</h2>
            
@@ -32,6 +34,7 @@ function Contact() {
            <span className='contact-icons'><a href='https://www.linkedin.com/in/dean-ward-92898b1a0/' className='contact-link'><i class="fab fa-linkedin linked"></i></a></span>
            <span className='contact-icons'><a href="mailto:dean.leeward@gmail.com" className='contact-link'><i class="fas fa-envelope-square email"></i></a></span>
            <span className='contact-icons'><a href="tel:07547986103" className='contact-link'><i class="fas fa-phone-square-alt phone"></i></a></span>
+        </div>
         </div>
         </div>
         
